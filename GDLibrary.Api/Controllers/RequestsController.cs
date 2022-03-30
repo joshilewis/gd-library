fc9de92223b078edecdd7a0fd84dd093ad46bc08
Joshua Lewis <joshua@forgood.co.za>
2022-03-30 22:06:25 +0200

    🐛 corrects the path of the API

--- GDLibrary.Api/Controllers/RequestsController.cs
+++ GDLibrary.Api/Controllers/RequestsController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GDLibrary.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/request")]
     [ApiController]
     public class RequestsController : ControllerBase
     {
